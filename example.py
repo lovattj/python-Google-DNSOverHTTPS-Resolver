92ba0c6d0d3443a1b43ef14af50f6216b396dc31
Jonathan Lovatt (MacPro) <lovattj@me.com>
2019-06-10 12:09:13 +0100

    update

--- example.py
+++ example.py
@@ -1,8 +1,7 @@
 import dnsOverHttps
 
-query = "cloudflare.com"
-
 print("*** Demonstration of creating a new query object and getting all A records for the domain ***")
+query = "google.com"
 gd = dnsOverHttps.GoogleDNSQuery(query,"a")
 
 try:
@@ -15,16 +14,18 @@ except Exception as e:
 
 print("\n*** Demonstration of convenience method to just get an IPv4 A address for a domain ***")
 
+import dnsOverHttps
 try:
-    a = dnsOverHttps.GoogleDNSQuery.getARecordFor(query)
-    print(a)
+  aRecord = dnsOverHttps.GoogleDNSQuery.getARecordFor("google.com")
+  print(aRecord) # IP Address here!
 except Exception as e:
-    print(type(e))
+  print("EXCEPTION - {} - {}".format(type(e),e))
 
 print("\n*** Demonstration of convenience method to just get an IPv6 AAAA address for a domain ***")
 
+import dnsOverHttps
 try:
-    a = dnsOverHttps.GoogleDNSQuery.getAAAARecordFor(query)
-    print(a)
+  aaaaRecord = dnsOverHttps.GoogleDNSQuery.getAAAARecordFor("google.com")
+  print(aaaaRecord) # IP Address here!
 except Exception as e:
-    print(type(e))
+  print("EXCEPTION - {} - {}".format(type(e),e))
--- readme.md
+++ readme.md
@@ -12,7 +12,7 @@ try:
   aRecord = dnsOverHttps.GoogleDNSQuery.getARecordFor("google.com")
   print(aRecord) # IP Address here!
 except Exception as e:
-  print("EXCEPTION - {} - {}".format(type(e),e)
+  print("EXCEPTION - {} - {}".format(type(e),e))
 ```
 
 If you just want a quick AAAA record, use the convenience method:
@@ -22,7 +22,7 @@ try:
   aaaaRecord = dnsOverHttps.GoogleDNSQuery.getAAAARecordFor("google.com")
   print(aaaaRecord) # IP Address here!
 except Exception as e:
-  print("EXCEPTION - {} - {}".format(type(e),e)
+  print("EXCEPTION - {} - {}".format(type(e),e))
 ```
 **Example**  
 See and run `example.py`
